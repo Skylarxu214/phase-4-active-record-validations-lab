ea808e3a6e23884b28585b55dbd58836b38714c2
Tian Xu <xueqingxu0302@gmail.com>
2022-02-02 19:12:05 -0700

    commit tree cf76e1aa6b8c54e4e2be872204a3ca32f084b0aa

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,22 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250}
+    validates :summary, length: { maximum: 250}
+    validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+    validate :clickbait
+    CLICKBAIT_PATTERNS = [
+        "Won't Believe",
+        "Secret",
+        "Top [number]",
+        "Guess"
+      ]
+    
+      def clickbait
+        unless CLICKBAIT_PATTERNS.none?{ |pat| pat == title }
+          errors.add(:title, "must be clickbait")
+        end
+    end
+  
+
+
 end
